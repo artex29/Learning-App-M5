01b12c1c4d1d0c8a6fa8a4da2cf14c4750f317b0
Angel <angelramirez@artexcomputer.com>
2022-01-05 19:21:18 -0500

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by ANGEL RAMIREZ on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
